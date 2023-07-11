ed0ac8569e497936e12ee1bbda367231fb2758c1
Anton Schirg <anton.schirg@soundskrit.ca>
2023-07-11 13:41:29 -0400

    PySide6 fix

--- NodeGraphQt/widgets/viewer_nav.py
+++ NodeGraphQt/widgets/viewer_nav.py
@@ -99,10 +99,10 @@ class NodeNavigationWidget(QtWidgets.QListView):
 
     def __init__(self, parent=None):
         super(NodeNavigationWidget, self).__init__(parent)
-        self.setSelectionMode(self.SingleSelection)
-        self.setResizeMode(self.Adjust)
-        self.setViewMode(self.ListMode)
-        self.setFlow(self.LeftToRight)
+        self.setSelectionMode(QtWidgets.QListView.SingleSelection)
+        self.setResizeMode(QtWidgets.QListView.Adjust)
+        self.setViewMode(QtWidgets.QListView.ListMode)
+        self.setFlow(QtWidgets.QListView.LeftToRight)
         self.setDragEnabled(False)
         self.setMinimumHeight(20)
         self.setMaximumHeight(36)
